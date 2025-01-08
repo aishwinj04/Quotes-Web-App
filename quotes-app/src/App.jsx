5868fb5b8c15189e16135d53e073189c77eb78f6
aishwinj04 <aishwin@gmail.com>
2025-01-07 20:55:19 -0500

    Initial Commit

---
+++ quotes-app/src/App.jsx
@@ -0,0 +1,10 @@
+import QuotesApp from "./Components/QuotesApp"
+import "./Components/QuotesApp"
+
+const App = () => {
+  return (
+    <div>QuotesApp</div>
+  )
+}
+
+export default App
---
+++ quotes-app/src/Components/QuotesApp.jsx
@@ -0,0 +1,7 @@
+const QuotesApp = () => {
+  return (
+    <div>QuotesApp</div>
+  )
+}
+
+export default QuotesApp
