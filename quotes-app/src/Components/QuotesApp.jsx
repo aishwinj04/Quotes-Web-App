232f99d10578857e7d1356c09a2b8b77b79e5174
aishwinj04 <aishwin@gmail.com>
2025-01-08 15:07:58 -0500

    Quote Fetching

--- quotes-app/src/Components/QuotesApp.jsx
+++ quotes-app/src/Components/QuotesApp.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react"
+
 const QuotesApp = () => {
+    const [quote, setQuote] = useState({
+        text: "Ask not what your country can do for you; ask what you can do for your country",
+        author: "John Kennedy"
+    })
+
+    const fetchNewQuote = async () => {
+        const url = "https://go-quote.azurewebsites.net/docs/#/default/get_random_quote" /* keyless api */
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        setQuote({
+            text:data.text,
+            author: data.author
+        })
+    }
   return (
     <div className="container">
         <div className="quote-container">
@@ -6,8 +23,8 @@ const QuotesApp = () => {
             <i className="bx bxs-heart fav-icon"></i>
             <div className="quote">
                 <i className="bx bxs-quote-alt-left left-quote"></i>
-                <p className="quote-text">Ask not what your country can do for you; ask what you can do for your country</p>
-                <p className="quote-author">John Kennedy</p>
+                <p className="quote-text">{quote.text}</p>
+                <p className="quote-author">{quote.author}</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
             <div className="circles">
@@ -17,7 +34,7 @@ const QuotesApp = () => {
                 <div className="circle-4"></div>
             </div>
             <div className="buttons">
-                <button className="btn btn-new">New Quote</button>
+                <button className="btn btn-new" onClick={fetchNewQuote}>New Quote</button>
                 <button className="btn btn-fav">Favourite</button>
                 
                 
