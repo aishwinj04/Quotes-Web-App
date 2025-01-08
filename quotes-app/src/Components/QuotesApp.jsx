4f8b2bed3afa3b3f83b401cb958ef13ca9ca7e7a
aishwinj04 <aishwin@gmail.com>
2025-01-07 21:18:54 -0500

    Update QuotesApp.jsx

--- quotes-app/src/Components/QuotesApp.jsx
+++ quotes-app/src/Components/QuotesApp.jsx
@@ -10,6 +10,18 @@ const QuotesApp = () => {
                 <p className="quote-author">John Kennedy</p>
                 <i className="bx bxs-quote-alt-right right-quote"></i>
             </div>
+            <div className="circles">
+                <div className="circle-1"></div>
+                <div className="circle-2"></div>
+                <div className="circle3"></div>
+                <div className="circle4"></div>
+            </div>
+            <div className="buttons">
+                <button className="btn btn-new">New Quote</button>
+                <button className="btn btn-fav">Favourite</button>
+                
+                
+            </div>
         </div>
     </div>
   )
